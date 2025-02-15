c4f054f153ee4b1f8b261f81eb287a365fceee39
Blonicx <Belte_YT@outlook.com>
2025-02-15 12:08:03 +0100

    Added Pre-Made Yellow Color

--- src/main/java/com/blonicx/basecore/api/utils/io/Color.java
+++ src/main/java/com/blonicx/basecore/api/utils/io/Color.java
@@ -2,8 +2,9 @@ package com.blonicx.basecore.api.utils.io;
 
 public class Color {
     public static final int RED = 0xFFFF0000;
-    public static final int GREEN = 0xFF00FF00;
     public static final int BLUE = 0xFF0000FF;
+    public static final int YELLOW = 0xFFFFFF00;
+    public static final int GREEN = 0xFF00FF00;
     public static final int WHITE = 0xFFFFFFFF;
     public static final int BLACK = 0xFF000000;
     public static final int TRANSPARENT = 0x00000000;
